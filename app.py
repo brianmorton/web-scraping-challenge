244509c937f4b8dd1784a8e0fa7d3388e770a442
Brian Morton <bmorton925@gmail.com>
2021-03-26 10:08:35 -0400

    part 2 file set up and boilerplate code

---
+++ app.py
@@ -0,0 +1,36 @@
+#app.py
+
+
+
+
+
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+
+app = Flask(__name__)
+
+
+# flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/phone_app"
+mongo = PyMongo(app)
+
+@app.route("/")
+def index():
+#    listings = mongo.db.listings.find_one()
+##    return render_template("index.html", listings=listings)
+
+
+@app.route("/scrape")
+def scraper():
+  #  listings = mongo.db.listings
+ #   listings_data = scrape_phone.scrape()
+ #   listings.update({}, listings_data, upsert=True)
+    return redirect("/", code=302)
+
+
+
+
+#end
+if __name__ == "__main__":
+    app.run(debug=True)
--- scrape_mars.py
+++ scrape_mars.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 # In[1]:
-
+def scrape():
 
 #imports
 import pandas as pd
